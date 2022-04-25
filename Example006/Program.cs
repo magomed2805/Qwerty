eb90178c9a632fdb54f5e3c22d677671d9e9cf21
Magomed <magamedovm398@gmail.com>
2022-04-25 15:33:22 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username == "Магомед")
+{
+    Console.WriteLine("Салам Алейкум Магомед");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
